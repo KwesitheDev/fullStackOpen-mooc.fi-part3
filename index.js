f93f9fe85c7baae5eab6350afb127a98f77dd980
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-04-01 22:17:37 +0100

    exercise 3.7 and 3.8

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+//creating a  morgan token
+morgan.token('body', (req) => {
+    return req.method === 'POST'  ? JSON.stringify(req.body) : ''
+})
+
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 let persons = [
     { 
       "id": "1",
